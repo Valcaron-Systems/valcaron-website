46571da974436ec044166c1a43bdabdc2cdbb8d5
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2026-01-24 16:12:35 +0000

    pain

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/valcaron-website/",
+export default defineConfig({
+  base: "/", // IMPORTANT for custom domain
   plugins: [react()],
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
